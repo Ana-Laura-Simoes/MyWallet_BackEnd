f03252392d3955a14187f18a6f9b6debe59587d4
Ana Simoes <ana.simoes-mato@unesp.br>
2021-06-25 02:08:54 -0300

    testes para sign up prontos

--- tests/SignUp.test.js
+++ tests/SignUp.test.js
@@ -2,10 +2,18 @@ import app from '../src/app.js';
 import supertest from 'supertest';
 import connection from '../src/database/database.js'
 
+beforeAll(async () => {
+    await connection.query(`DELETE FROM users`);
+  });
+
+afterAll(() => {
+    connection.end();
+});
+
 describe("POST /signUp", () => {
     it("returns 201 for valid params", async () => {
         const body = {
-            name: 'josi',
+            name: 'joaozinho',
             email: 'j.@gmail.com',
             password: '1',
             confirmPassword:'1'
@@ -15,5 +23,48 @@ describe("POST /signUp", () => {
           const status = result.status;
           
           expect(status).toEqual(201);
+    });
+
+
+    it("returns 400 for invalid params", async () => {
+        const body = {
+            name: 'joaozinho',
+            email: 'j.@gmail.com',
+            password: '',
+            confirmPassword:'3'
+          };
+        
+          const result = await supertest(app).post("/signUp").send(body);
+          const status = result.status;
+          
+          expect(status).toEqual(400);
+    });
+    
+    it("returns 400 for invalid params", async () => {
+        const body = {
+            name: 'joaozinho',
+            email: 'j.@gmail.com',
+            password: '1',
+            confirmPassword:'3'
+          };
+        
+          const result = await supertest(app).post("/signUp").send(body);
+          const status = result.status;
+          
+          expect(status).toEqual(400);
+    });
+
+    it("returns 409 for duplicated email", async () => {
+        const body = {
+            name: 'juju',
+            email: 'j.@gmail.com',
+            password: '3',
+            confirmPassword:'3'
+          };
+        
+          const result = await supertest(app).post("/signUp").send(body);
+          const status = result.status;
+          
+          expect(status).toEqual(409);
     });
 });
